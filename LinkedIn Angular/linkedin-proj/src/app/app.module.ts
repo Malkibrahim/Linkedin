6090b0e10ed33562f71e084c767e4e9dcffd634e
Mariam Magdy <magdymariam@gmail.com>
2020-03-27 08:07:39 +0200

    angular component

--- LinkedIn Angular/linkedin-proj/src/app/app.module.ts
+++ LinkedIn Angular/linkedin-proj/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NavigationComponent } from "./layout/navigation/navigation.component";
+import { UserInfoComponent } from "./features/profile/user-info/user-info.component";
+import { from } from "rxjs";
+import { SectionComponent } from './shared/section/section.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavigationComponent, UserInfoComponent, SectionComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
