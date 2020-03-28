d30d09189e0a391361a378ae988be1a35cd272cf
Mariam Magdy <magdymariam@gmail.com>
2020-03-28 16:50:54 +0200

    routing

--- LinkedIn Angular/linkedin-proj/src/app/app.module.ts
+++ LinkedIn Angular/linkedin-proj/src/app/app.module.ts
@@ -1,15 +1,39 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { NavigationComponent } from "./layout/navigation/navigation.component";
 import { UserInfoComponent } from "./features/profile/user-info/user-info.component";
 import { from } from "rxjs";
-import { SectionComponent } from './shared/section/section.component';
+import { SectionComponent } from "./shared/section/section.component";
+import { AddSectionComponent } from "./features/profile/add-section/add-section.component";
+import { CreatePostComponent } from "./features/post/create-post/create-post.component";
+import { AddPostComponent } from "./features/post/add-post/add-post.component";
+import { AddCommentComponent } from "./features/post/add-comment/add-comment.component";
+import { ErrorPageComponent } from "./layout/error-page/error-page.component";
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, UserInfoComponent, SectionComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    UserInfoComponent,
+    SectionComponent,
+    AddSectionComponent,
+    CreatePostComponent,
+    AddPostComponent,
+    AddCommentComponent,
+    ErrorPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "", redirectTo: "/home", pathMatch: "full" },
+      { path: "home", component: CreatePostComponent },
+      { path: "profile", component: UserInfoComponent },
+      { path: "**", component: ErrorPageComponent }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
